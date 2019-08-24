cd7702e6397a048da59342d4acec720394b8e7f9
Shadowsych <pravat.bhusal@gmail.com>
2019-08-23 18:34:17 -0600

    Better explain contains on a String key and use constant for MODE_PRIVATE

--- Lecture7/app/src/main/java/com/mad/lecture7/MainActivity.java
+++ Lecture7/app/src/main/java/com/mad/lecture7/MainActivity.java
@@ -2,20 +2,20 @@ package com.mad.lecture7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // receive the SharedPreferences and its Editor for "Lecture7" using private mode (0)
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("Lecture7", 0);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("Lecture7", Context.MODE_PRIVATE);
         Editor editor = pref.edit();
 
         // store a String called "name" with value "Pravat Bhusal"
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // commit changes
         editor.commit();
 
-        // retrieve the String key "name", use a default value of null
+        // retrieve the String key "name" use a default value of null if it does not exist
         String name = pref.getString("name", null);
         Log.d("name1", name);
 
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
         editor.remove("name");
         editor.commit();
 
-        // now try and retrieve the String key "name", it should return the default value of null
-        name = pref.getString("name", null);
-        Log.d("name2", name);
+        // now try and check if the String key "name" exists, so this should return false
+        boolean nameExists = pref.contains("name");
+        Log.d("name_exists", "" + nameExists);
 
         // clear all data, then commit changes
         editor.clear();
